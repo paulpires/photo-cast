3ed1f4bd3074a512b6bd9e1e0a80ee33213ab5b9
paulpires <paulpires101@gmail.com>
2020-02-23 11:42:20 +0000

    Install Firebase/Analytics via cocoapods

---
+++ Pods/Target Support Files/Pods-photo-cast-photo-castUITests/Pods-photo-cast-photo-castUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_photo_cast_photo_castUITests : NSObject
+@end
+@implementation PodsDummy_Pods_photo_cast_photo_castUITests
+@end
---
+++ Pods/Target Support Files/Pods-photo-castTests/Pods-photo-castTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_photo_castTests : NSObject
+@end
+@implementation PodsDummy_Pods_photo_castTests
+@end
