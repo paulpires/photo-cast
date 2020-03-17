7fdab1293795398f89881cb1f54f20fcd3348518
paulpires <paulpires101@gmail.com>
2020-03-17 09:36:05 +0000

    test

--- scripts/install_certs_repo_deploy_key.sh
+++ scripts/install_certs_repo_deploy_key.sh
@@ -5,6 +5,7 @@
 
 set -e
 
+eval $(ssh-agent -s)
 openssl aes-256-cbc \
     -K $encrypted_1d562ea9f9b4_key \
     -iv $encrypted_1d562ea9f9b4_iv \
@@ -12,5 +13,4 @@ openssl aes-256-cbc \
     -out github_certs_deploy_key \
     -d
 chmod 600 github_certs_deploy_key
-eval $(ssh-agent -s)
 ssh-add -K github_certs_deploy_key
