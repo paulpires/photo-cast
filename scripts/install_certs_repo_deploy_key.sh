1c8821209127fdf494ae4273dbcaa35c93e7b311
paulpires <paulpires101@gmail.com>
2020-03-17 09:19:42 +0000

    updated deploy key

--- scripts/install_certs_repo_deploy_key.sh
+++ scripts/install_certs_repo_deploy_key.sh
@@ -6,11 +6,11 @@
 set -e
 
 openssl aes-256-cbc \
-    -K $encrypted_667e26f82b63_key \
-    -iv $encrypted_667e26f82b63_iv \
+    -K $encrypted_1d562ea9f9b4_key \
+    -iv $encrypted_1d562ea9f9b4_iv \
     -in ./scripts/github_certs_deploy_key.enc \
-    -out deployment_key \
+    -out github_certs_deploy_key \
     -d
-chmod 600 deployment_key
+chmod 600 github_certs_deploy_key
 eval $(ssh-agent -s)
-ssh-add deployment_key
+ssh-add github_certs_deploy_key
